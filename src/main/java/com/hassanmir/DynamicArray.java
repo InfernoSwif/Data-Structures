9e817774656515fb33e025e1c5f855f936eee6be
InfernoSwif <31957132+InfernoSwif@users.noreply.github.com>
2020-12-25 21:32:41 +0000

    Adding Dynamic Array and corresponding Test Cases

--- src/main/java/com/hassanmir/DynamicArray.java
+++ src/main/java/com/hassanmir/DynamicArray.java
@@ -1,9 +1,9 @@
 package com.hassanmir;
 
 public class DynamicArray<E> {
-    E[] array;
-    int capacity;
-    int size;
+    private E[] array;
+    private int capacity=0;
+    private int size=0;
 
     public DynamicArray(int capacity){
         if(capacity<1)
@@ -16,8 +16,87 @@ public class DynamicArray<E> {
         new DynamicArray(1);
     }
 
-    public static void main(String[] args){
-        DynamicArray<Integer> arr = new DynamicArray<>(5);
-        System.out.println("capacity is: " + arr.capacity);
+    public int size(){
+        return size;
     }
+
+    public int capacity(){
+        return capacity;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    //Double the capacity each time the capacity is reached
+    public void add(E element){
+        if(size==capacity){
+            capacity *=2;
+            E[] array2 = (E[]) new Object[capacity];
+            for(int i=0;i<size;i++){
+                array2[i]=array[i];
+            }
+            array2[size]=element;
+            array=array2;
+        }else{
+            array[size]=element;
+        }
+        size++;
+    }
+
+    public E get(int index){
+        if(index<0 || index>size) throw new IndexOutOfBoundsException();
+        return array[index];
+    }
+
+    public void set(int index, E element){
+        if(index<0 || index>size) throw new IndexOutOfBoundsException();
+        array[index] = element;
+    }
+
+    public int indexOf(Object obj){
+        for(int i=0;i<size;i++){
+            if(array[i].equals(obj))
+                return i;
+        }
+        return -1;
+    }
+
+    //Iterate through till removing index is reached then skip
+    public void removeAt(int index){
+        E[] array2 = (E[]) new Object[capacity*2];
+        int j=0;
+        for(int i=0;i<size;i++){
+            if(i!=index) {
+                array2[j] = array[i];
+                j++;
+            }
+        }
+        array=array2;
+        size--;
+    }
+
+    public boolean remove(Object obj){
+        int index = indexOf(obj);
+        if(index!=-1){
+            removeAt(index);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean contains(Object obj){
+        if(indexOf(obj)==-1) return false;
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        if(size==0) return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i=0;i<size-1;i++) sb.append(array[i] + ",");
+        return sb.append(array[size-1]+"]").toString();
+    }
+
 }
--- src/test/java/com/hassanmir/DynamicArrayTest.java
+++ src/test/java/com/hassanmir/DynamicArrayTest.java
@@ -6,9 +6,131 @@ import org.junit.Test;
 public class DynamicArrayTest {
 
     @Test
-    public void DynamicArrayTest(){
-        DynamicArray<Integer> arr = new DynamicArray<>(5);
-        Assert.assertTrue(arr.capacity==5);
-        Assert.assertTrue(true);
+    public void testCapacity(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        Assert.assertEquals(3,arr.capacity());
+        arr.add(123);
+        arr.add(123);
+        arr.add(123);
+        Assert.assertEquals(3,arr.capacity());
+        arr.add(123);
+        Assert.assertEquals(6,arr.capacity());
+    }
+
+    @Test
+    public void testSize(){
+        DynamicArray<String> arr = new DynamicArray<>(3);
+        Assert.assertEquals(0,arr.size());
+        arr.add("abc");
+        arr.add("abc");
+        arr.add("abc");
+        Assert.assertEquals(3,arr.size());
+        arr.add("abc");
+        Assert.assertEquals(4,arr.size());
+    }
+
+    @Test
+    public void testIsEmpty(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        Assert.assertTrue(arr.isEmpty());
+        arr.add(1);
+        Assert.assertFalse(arr.isEmpty());
+    }
+
+    @Test
+    public void testGet(){
+        DynamicArray<String> arr = new DynamicArray<>(3);
+        arr.add("abc");
+        arr.add("def");
+        arr.add("ghi");
+        Assert.assertEquals(arr.get(1),"def");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetOutOfBoundsException1(){
+        DynamicArray<String> arr = new DynamicArray<>();
+        arr.get(1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetOutOfBoundsException2(){
+        DynamicArray<String> arr = new DynamicArray<>();
+        arr.set(5,"abc");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetOutOfBoundsException3(){
+        DynamicArray<String> arr = new DynamicArray<>();
+        arr.add("abc");
+        arr.get(-3);
+    }
+
+    @Test
+    public void testSet(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        arr.add(100);
+        arr.add(101);
+        arr.add(102);
+        Assert.assertEquals(101, (int) arr.get(1));
+        arr.set(1,201);
+        Assert.assertEquals(201, (int) arr.get(1));
+    }
+
+    @Test
+    public void testIndexOf(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        arr.add(100);
+        arr.add(101);
+        arr.add(102);
+        Assert.assertEquals(1, arr.indexOf(101));
+        Assert.assertEquals(-1, arr.indexOf(103));
+    }
+
+    @Test
+    public void testRemoveAt(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        arr.add(100);
+        arr.add(101);
+        arr.add(102);
+        Assert.assertEquals(101,(int)arr.get(1));
+        arr.removeAt(1);
+        Assert.assertEquals(102,(int)arr.get(1));
+    }
+
+    @Test
+    public void testRemove(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        arr.add(100);
+        arr.add(101);
+        arr.add(102);
+        Assert.assertEquals(1,arr.indexOf(101));
+        arr.remove(101);
+        Assert.assertEquals(-1,arr.indexOf(101));
+    }
+
+    @Test
+    public void testContains(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        arr.add(100);
+        arr.add(101);
+        arr.add(102);
+        Assert.assertTrue(arr.contains(101));
+        arr.remove(101);
+        Assert.assertFalse(arr.contains(101));
+    }
+
+    @Test
+    public void testToString(){
+        DynamicArray<Integer> arr = new DynamicArray<>(3);
+        arr.add(100);
+        arr.add(101);
+        arr.add(102);
+        Assert.assertEquals("[100,101,102]",arr.toString());
+
+        DynamicArray<String> arrStr = new DynamicArray<>(3);
+        arrStr.add("100");
+        arrStr.add("101");
+        arrStr.add("102");
+        Assert.assertEquals("[100,101,102]",arrStr.toString());
     }
 }
